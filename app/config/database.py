21ba7fda59172a31745ddbe72b7efb150dd6584a
Harshit <harshit392615@gmail.com>
2026-07-06 23:57:17 +0530

    feat : add database enginer , db session maker , basemodel

---
+++ app/config/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine , AsyncSession , async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from settings import settings
+
+engine = create_async_engine(
+    settings.DATABASE_URL
+)
+
+Async_Session_Maker = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+Base_Model = declarative_base()
+
+async def get_db_connection():
+    async with Async_Session_Maker() as session : 
+        yield session
