b7458699fe97e4ed126a0e7c372126c95e4c261d
Harshit <harshit392615@gmail.com>
2026-07-06 23:57:43 +0530

    feat : add user model

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from app.config.database import Base_Model
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column , String , DateTime , func , ForeignKey
+import uuid
+from sqlalchemy.orm import validates
+import bcrypt
+
+class User(Base_Model):
+    id = Column(UUID , nullable=False , primary_key=True ,  default=uuid.uuid4)
+    name = Column(String , nullable=False)
+    email = Column(String , nullable=False , unique=True)
+    password = Column(String , nullable=False)
+    created_at = Column(DateTime , server_default=func.now())
+
+    @validates("password")
+    def hash_password(self , key , value):
+        salt = bcrypt.gensalt()
+        hashed_pw = bcrypt.hashpw(value.encode("utf-8") , salt=salt)
+        return hashed_pw.decode("utf-8")
+    
+    def verify_pw(self , unhashed_pw:str):
+        return bcrypt.checkpw(unhashed_pw.encode("utf-8") , self.password.encode("utf-8"))
