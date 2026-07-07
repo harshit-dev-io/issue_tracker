746e7a1906e8ab25b2d634a26b09d195f3fd4591
Harshit <harshit392615@gmail.com>
2026-07-07 12:33:47 +0530

    feat : add workspace and membership , Board model

--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
 from app.config.database import Base_Model
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column , String , DateTime , func , ForeignKey
+from sqlalchemy import Column , String , DateTime , func , ForeignKey , Enum , Table , Boolean
 import uuid
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates , relationship
 import bcrypt
+import enum
 
 class User(Base_Model):
+    __tablename__ = "user"
+
     id = Column(UUID , nullable=False , primary_key=True ,  default=uuid.uuid4)
     name = Column(String , nullable=False)
     email = Column(String , nullable=False , unique=True)
@@ -20,3 +23,26 @@ class User(Base_Model):
     
     def verify_pw(self , unhashed_pw:str):
         return bcrypt.checkpw(unhashed_pw.encode("utf-8") , self.password.encode("utf-8"))
+    
+class Workspace(Base_Model):
+    __tablename__ = "workspace"
+    id = Column(UUID , nullable=False , default=uuid.uuid4)
+    name = Column(String , nullable=False)
+    created_at = Column(DateTime , server_default=func.now())
+
+class Membership(Base_Model):
+    __tablename__ = "membership"
+    id = Column(UUID , nullable=False , default=uuid.uuid4)
+    user = Column(UUID , ForeignKey("user.id" , ondelete="CASCADE") , nullable=False)
+    workspace = Column(UUID , ForeignKey("workspace.id" , ondelete="CASCADE") , nullable=False)
+    role = Column(String , nullable=False )
+    created_at = Column(DateTime , server_default=func.now())
+
+class Board(Base_Model):
+    __tablename__ = "board"
+    id = Column(UUID , nullable=False , default=uuid.uuid4)
+    name = Column(String , nullable=False)
+    owner = Column(UUID , ForeignKey("user.id" , ondelete="CASCADE") , nullable=False)
+    workspace = Column(UUID , ForeignKey("workspace.id" , ondelete="CASCADE") , nullable=False)
+    created_at = Column(DateTime , server_default=func.now())
+
