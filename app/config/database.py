69597adb1c942c8865f27a99899874ec8998e1d7
Harshit <harshit392615@gmail.com>
2026-07-07 15:23:37 +0530

    fix :  import error

--- app/config/database.py
+++ app/config/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine , AsyncSession , async_sessionmaker
 from sqlalchemy.orm import declarative_base
-from settings import settings
+from app.config.settings import settings
 
 engine = create_async_engine(
     settings.DATABASE_URL
