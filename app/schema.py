760cbf442336e4fdfd627c6d94d0c4c7ef0324bf
Harshit <harshit392615@gmail.com>
2026-07-07 22:09:30 +0530

    feat : add schema class for issue , sub-issue , label , assignee

--- app/schema.py
+++ app/schema.py
@@ -1,5 +1,8 @@
-from pydantic import BaseModel , EmailStr
+from pydantic import BaseModel , EmailStr 
 from uuid import UUID
+from typing import List
+from app import models
+from datetime import datetime
 
 class Signup(BaseModel):
     name : str
@@ -31,3 +34,44 @@ class Board_create(BaseModel):
 class Board(BaseModel):
     id : UUID
     name : str
+
+class Issue_create(BaseModel):
+    name : str
+    board_id : UUID
+    label : List[UUID]
+    assignees : List[UUID] = []
+    content : str
+    
+class Label(BaseModel):
+    id : UUID
+    name : str
+    type : models.LABEL_TYPE
+
+class Label_create(BaseModel):
+    name : str
+    workspace_id : UUID
+
+class Sub_Issue_create(BaseModel):
+    name : str
+    issue_id : UUID
+    content : str
+
+class Sub_Issue(BaseModel):
+    id : UUID
+    name : str
+    content : str
+    is_completed : bool
+
+class Assignees(BaseModel):
+    id : UUID
+    name : str
+    
+class Issue(BaseModel):
+    id : UUID
+    name : str
+    label : List[Label] = []
+    sub_issues : List[Sub_Issue] = []
+    assignees : List[Assignees] = []
+    content : str
+    status : models.STATUS
+    created_at : datetime
