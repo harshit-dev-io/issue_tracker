332f32d09a903d218e4207e9b907725de5cf0c86
Harshit <harshit392615@gmail.com>
2026-07-07 12:39:19 +0530

    feat : add signup , login , token response schema classes

---
+++ app/schema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel , EmailStr
+
+class Signup(BaseModel):
+    name : str
+    email : EmailStr
+    password : str
+
+class Login(BaseModel):
+    email : EmailStr
+    password : str
+
+class Token_Response(BaseModel):
+    access_token : str
+    refresh_token : str 
