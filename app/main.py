3c2ce10e5b80a7e8d4db33c6423a7a6e53d1f410
Harshit <harshit392615@gmail.com>
2026-07-07 15:46:37 +0530

    feat : add server configuration for cors middleware , routes , table generation on startup

--- app/main.py
+++ app/main.py
@@ -1,4 +1,24 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI , Depends 
+from app.router import router
+from app.config.database import engine , Base_Model
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware , 
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_headers = ["*"],
+    allow_methods = ["*"]
+)
+
+app.include_router(router=router)
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as db:
+        await db.run_sync(Base_Model.metadata.create_all)
+    
