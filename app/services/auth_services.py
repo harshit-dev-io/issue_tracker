e9efeb7c7174c9d38d6ce86f344416efeac0fa19
Harshit <harshit392615@gmail.com>
2026-07-07 15:44:35 +0530

    feat : add Get_token function for token generation , Verify_access_token function for access token verification

---
+++ app/services/auth_services.py
@@ -0,0 +1,34 @@
+import jwt 
+from sqlalchemy import select 
+from app import models
+from datetime import datetime , timezone , timedelta
+from app.config.settings import settings
+
+def Get_token(user_id):
+    payload = {
+        "sub" : str(user_id),
+        "exp" : datetime.now(timezone.utc)+timedelta(minutes=15),
+        "type" : "access"
+    }
+
+    access_token = jwt.encode(payload , settings.JWT_SECRET , algorithm="HS256")
+    
+    payload["exp"] = datetime.now(timezone.utc)+timedelta(days=7)
+    payload["type"] = "refresh"
+
+    refresh_token = jwt.encode(payload , settings.JWT_SECRET , algorithm="HS256")
+
+    refresh = models.Refresh_Token(
+        refresh_token = refresh_token,
+        user_id = user_id,
+        expire_on = payload["exp"]
+    )
+
+    return {"access_token" : access_token , "refresh_token" : refresh_token} , refresh
+
+def Verify_access_token(access_token):
+    try:
+        payload = jwt.decode(access_token , settings.JWT_SECRET , algorithms="HS256")
+        return payload
+    except :
+        return None    
